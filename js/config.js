a259f1384c97a1219eb3a42d7131f08589c0f9a1
tunlyin <tunlyinaung.mmocc@gmail.com>
2023-11-13 08:25:31 +0630

    modify config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_IfqJBWHBV', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '71ikup88e25puvm7ccjmlmioqn', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: '', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://nr5q68m0n9.execute-api.us-east-1.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
